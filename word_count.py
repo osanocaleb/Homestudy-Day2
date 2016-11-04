6ade83c43518a1ac7ff7090963500d8f818bab3a
osanocaleb <caleb.osano.co@gmail.com>
2016-11-04 11:13:43 +0300

    added a word count file

---
+++ word_count.py
@@ -0,0 +1,18 @@
+def words(string):
+	if type(string) == str:
+		split_string = string.split()
+		string_dict = {}
+		for word in split_string:
+			if word in string_dict:
+				if word.isdigit() == True: 
+					word = int(word)
+					string_dict[word] += 1
+				else:
+					string_dict[word] += 1
+			else:
+				if word.isdigit() == True:
+					word = int(word)
+					string_dict[word] = 1
+				else:
+					string_dict[word] = 1
+		return string_dict
